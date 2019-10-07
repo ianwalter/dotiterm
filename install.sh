94d1101a49eb77702ac45ae61edfe221e9184bfe
Ian Walter <public@iankwalter.com>
2019-10-07 13:53:22 -0400

    Trying to get Profiles to work correctly

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Copy preferences file.
 cp ./com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
 
+# Copy profile configuration.
+cp Profiles.json ~/Library/Application\ Support/iTerm2/DynamicProfiles
+
+# Download iTerm2 shell integration.
+if ! [ -e "${HOME}/.iterm2_shell_integration.zsh" ]; then
+  curl -L https://iterm2.com/shell_integration/install_shell_integration.sh | bash
+fi
+
 if [[ $? == 0 ]]; then
-  printf "\n✅ Installed iTerm 2 configuration successfully.\n"
+  printf "\n✅ Installed iTerm2 configuration successfully.\n"
 fi
