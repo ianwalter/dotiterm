25ac0f5e17ddf4dd1c94869712003a8bfcde28da
Ian Walter <public@iankwalter.com>
2019-10-07 14:05:17 -0400

    Removing separate profile logic;

--- install.sh
+++ install.sh
@@ -3,9 +3,6 @@
 # Copy preferences file.
 cp ./com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
 
-# Copy profile configuration.
-cp Profiles.json ~/Library/Application\ Support/iTerm2/DynamicProfiles
-
 # Download iTerm2 shell integration.
 if ! [ -e "${HOME}/.iterm2_shell_integration.zsh" ]; then
   curl -L https://iterm2.com/shell_integration/install_shell_integration.sh | bash
